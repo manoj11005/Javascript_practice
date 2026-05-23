b5553ec00a6e9bea5eca15386fc331bdbc141e4a
Manoj Kumar <2023eeb1223@iitrpr.ac.in>
2026-05-23 16:26:46 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+// treat all js code as newer version
+console.log(3+3)
+console.log("Manoj")
+let name="Preeti"
+let age=21
+let isLoggedIn=false
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined => 
+// symbol => unique
+// object
+console.log(typeof "Manoj")
+console.log(typeof null)
+// type is objest
+console.log(typeof undefined)
+// type is undefined
+
+console.table([name,age,isLoggedIn])
