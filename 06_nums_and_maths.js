f122d67d0d2fc95a33c228a9af9ded6170d6258e
Manoj Kumar <2023eeb1223@iitrpr.ac.in>
2026-05-25 11:27:56 +0530

    nums_and_maths

---
+++ 06_nums_and_maths.js
@@ -0,0 +1,32 @@
+const score=400
+// console.log(score);
+const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+const otherNumber=123.89
+// console.log(otherNumber.toPrecision(4));
+const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ***********     MATHS      *************
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(5.9));
+// console.log(Math.min(3,4,9,1));
+// console.log(Math.max(3,4,12,2));
+
+console.log(Math.random());
+// random values between 0 and 1 always
+console.log((Math.random()*10)+1);
+// as some value is 0.002 someyhing after multiplying by 10
+// that will be less than 1 so i added one to keep 
+// value greater than 1 always
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min)+1)+min);
+// always value greater than or equal to 10 
