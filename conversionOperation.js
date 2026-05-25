59420fea80cff3fc55e2e7632881a6ab2c07c0d7
Manoj Kumar <2023eeb1223@iitrpr.ac.in>
2026-05-25 07:31:04 +0530

    conversion

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,21 +1,45 @@
-let score="33abc"
-console.log(typeof score);
-console.log(typeof(score));
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let score="33abc"
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 // conversions
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false=0;
 
-let isLoggedIn="Manoj"
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn="Manoj"
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 // 1=>true, 0=>false;
 // ""=>false
 // "Manoj" => true;
-let someNumber=33;
-let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber=33;
+// let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ******* OPERATIONS *****//
+// let value=3;
+// let negValue=-value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// let str1="hello"
+// let str2=" hitesh"
+// let str3=str1+str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+let gameCounter=100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
