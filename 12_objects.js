95b5703e3e175caa24de6a40f628ea772d443d19
Manoj Kumar <2023eeb1223@iitrpr.ac.in>
2026-05-27 12:40:04 +0530

    12_objects

---
+++ 12_objects.js
@@ -0,0 +1,81 @@
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+// course.courseInstructor
+const{courseInstructor}=course
+const{courseInstructor:ins}=course
+// console.log(courseInstructor);
+// console.log(ins);
+
+// JSON
+// "name":"manoj"
+// "coursename":"js in hindi"
+// "price":"free"
+
+[
+    {},
+    {},
+    {}
+]
+
+{
+  "results": [
+    {
+      "gender": "male",
+      "name": {
+        "title": "Mr",
+        "first": "Lonnie",
+        "last": "Martinez"
+      },
+      "location": {
+        "street": {
+          "number": 1830,
+          "name": "Crockett St"
+        },
+        "city": "Albany",
+        "state": "South Australia",
+        "country": "Australia",
+        "postcode": 5431,
+        "coordinates": {
+          "latitude": "-82.1678",
+          "longitude": "-96.5788"
+        },
+        "timezone": {
+          "offset": "-11:00",
+          "description": "Midway Island, Samoa"
+        }
+      },
+      "email": "lonnie.martinez@example.com",
+      "login": {
+        "uuid": "a802208a-75ab-460d-a4c3-0e1502351a6b",
+        "username": "silverdog135",
+        "password": "domingo",
+        "salt": "ExPSybYz",
+        "md5": "a27872873ce3dfd5024213f686b2e2a0",
+        "sha1": "e36a2797766ec006640277a318bab94aeb577b5d",
+        "sha256": "1f77b81e58b6758713bc89236262a00a014cd24d549c4e7c2e07ca43f7120b5b"
+      },
+      "dob": {
+        "date": "1996-08-23T17:56:23.096Z",
+        "age": 29
+      },
+      "registered": {
+        "date": "2003-07-15T12:08:10.349Z",
+        "age": 22
+      },
+      "phone": "00-1230-6478",
+      "cell": "0475-145-023",
+      "id": {
+        "name": "TFN",
+        "value": "471845491"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/35.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/35.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/35.jpg"
+      },
+      "nat": "AU"
+    }
+}
