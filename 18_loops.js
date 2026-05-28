2c6fc6f8abf0222f7b9197f5917b5369db55349e
Manoj Kumar <2023eeb1223@iitrpr.ac.in>
2026-05-28 12:39:54 +0530

    18_loops

---
+++ 18_loops.js
@@ -0,0 +1,32 @@
+// for(let i=0; i<=10; i++){
+//     // console.log(i);
+// }
+// for(let i=0; i<=5; i++){
+//     for(let j=0; j<=5; j++){
+//         console.log(i,j);
+//     }
+// }
+
+
+// WHILE DO WHILE LOOP
+
+// let index=0;
+// while(index<=10){
+//     console.log(`Value of index is ${index}`);
+//     index=index+2;
+// }
+
+// let myArr=["flash","batman","superman"]
+// let ar=0;
+// while(ar<myArr.length){
+//     console.log(`value is ${myArr[ar]}`);
+//     ar++;
+// }
+
+// let score=1;
+let score=11;
+do{
+    console.log(`score is: ${score}`);
+    score++;
+}while(score<10);
+//  do pahle execute hota hai phir condition check krta hai whhile me
